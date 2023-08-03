3b7ed625d6ebce40b4aecf0794a2050c3182b7b0
ghjattu <icracked369@gmail.com>
2023-08-03 14:09:05 +0800

    ✨ feat: login

---
+++ controllers/logincontroller.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"github.com/Ghjattu/tiny-tiktok/services"
+	"github.com/gin-gonic/gin"
+)
+
+type LoginResponse struct {
+	Response
+	UserID int64  `json:"user_id"`
+	Token  string `json:"token"`
+}
+
+func Login(c *gin.Context) {
+	username := c.Query("username")
+	password := c.Query("password")
+
+	ls := &services.LoginService{}
+	userID, statusCode, statusMsg, token := ls.Login(username, password)
+
+	// Login successfully.
+	if statusCode == 0 {
+		c.JSON(200, LoginResponse{
+			Response: Response{
+				StatusCode: statusCode,
+				StatusMsg:  statusMsg,
+			},
+			UserID: userID,
+			Token:  token,
+		})
+		return
+	}
+
+	c.JSON(200, LoginResponse{
+		Response: Response{
+			StatusCode: statusCode,
+			StatusMsg:  statusMsg,
+		},
+	})
+}

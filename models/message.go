ecaa0ac0ccd55cacdb3c38ac71412d830ebbbfc5
ghjattu <icracked369@gmail.com>
2023-08-15 21:29:16 +0800

    ✨ feat: get message list

--- models/message.go
+++ models/message.go
@@ -10,6 +10,14 @@ type Message struct {
 	CreateDate time.Time `gorm:"type:datetime;not null"`
 }
 
+type MessageDetail struct {
+	ID         int64  `json:"id"`
+	SenderID   int64  `json:"from_user_id"`
+	ReceiverID int64  `json:"to_user_id"`
+	Content    string `json:"content"`
+	CreateTime string `json:"create_time"`
+}
+
 // CreateNewMessage create new message.
 //
 //	@param m *Message
@@ -19,4 +27,21 @@ func CreateNewMessage(m *Message) (*Message, error) {
 	err := db.Model(&Message{}).Create(m).Error
 
 	return m, err
+}
+
+// GetMessageList get message list between sender and receiver.
+//
+//	@param senderID int64
+//	@param receiverID int64
+//	@return []Message
+//	@return error
+func GetMessageList(senderID, receiverID int64) ([]Message, error) {
+	messages := make([]Message, 0)
+
+	err := db.Model(&Message{}).
+		Where("sender_id = ? AND receiver_id = ?", senderID, receiverID).
+		Or("sender_id = ? AND receiver_id = ?", receiverID, senderID).
+		Find(&messages).Error
+
+	return messages, err
 }
--- services/messageservice.go
+++ services/messageservice.go
@@ -48,3 +48,54 @@ func (ms *MessageService) CreateNewMessage(senderID, receiverID int64, content s
 
 	return 0, "create new message successfully"
 }
+
+// GetMessageList gets the message list between the sender and the receiver.
+//
+//	@receiver ms *MessageService
+//	@param senderID int64
+//	@param receiverID int64
+//	@return int32 "status code"
+//	@return string "status message"
+//	@return []models.MessageDetail
+func (ms *MessageService) GetMessageList(senderID, receiverID int64) (int32, string, []models.MessageDetail) {
+	// Check if the receiver exists.
+	_, err := models.GetUserByUserID(receiverID)
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return 1, "receiver does not exist", nil
+		}
+		return 1, "failed to check receiver existence", nil
+	}
+
+	// Get the message list.
+	messageList, err := models.GetMessageList(senderID, receiverID)
+	if err != nil {
+		return 1, "failed to get message list", nil
+	}
+
+	// Convert the message list to message detail list.
+	messageDetailList := convertMessageToMessageDetail(messageList)
+
+	return 0, "get message list successfully", messageDetailList
+}
+
+// convertMessageToMessageDetail converts the message list to message detail list.
+//
+//	@param messageList []models.Message
+//	@return []models.MessageDetail
+func convertMessageToMessageDetail(messageList []models.Message) []models.MessageDetail {
+	messageDetailList := make([]models.MessageDetail, 0, len(messageList))
+
+	for _, message := range messageList {
+		messageDetail := models.MessageDetail{
+			ID:         message.ID,
+			SenderID:   message.SenderID,
+			ReceiverID: message.ReceiverID,
+			Content:    message.Content,
+			CreateTime: message.CreateDate.Format("2006-01-02 15:04:05"),
+		}
+		messageDetailList = append(messageDetailList, messageDetail)
+	}
+
+	return messageDetailList
+}

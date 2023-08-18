98b839455bcc10ef7d8c610d7d794b1c9b6b85fc
ghjattu <icracked369@gmail.com>
2023-08-18 17:39:42 +0800

    🎈 perf: save user information in cache

---
+++ middleware/redis/constants.go
@@ -0,0 +1,18 @@
+package redis
+
+const (
+	// Hash user:<user_id> save the user's information.
+	UserKey = "user:"
+
+	// Hash video:<video_id> save the video's information.
+	VideoKey = "video:"
+	// List favorite_videos:<user_id> save the user's favorite videos id.
+	FavoriteVideosKey = "favorite_videos:"
+	// List videos:author:<author_id> save the author's videos id.
+	VideosByAuthorKey = "videos:author:"
+
+	// List following:<user_id> save the user's following user id.
+	FollowingKey = "following:"
+	// List follower:<user_id> save the user's follower user id.
+	FollowerKey = "follower:"
+)

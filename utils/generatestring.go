ac93df3485eb659a3113c66c2e2512abbc3bb2bc
ghjattu <icracked369@gmail.com>
2023-09-01 15:38:11 +0800

    ✨ feat: generate random string

---
+++ utils/generatestring.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+func GenerateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyz"
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	randomString := make([]byte, length)
+	for i := range randomString {
+		randomString[i] = charset[r.Intn(len(charset))]
+	}
+
+	return string(randomString)
+}
